c71cd2a7729ede5565ad62e9fdba5c84e00207e1
vicenticko <v.gilbert@duocuc.cl>
2024-10-11 17:01:04 -0300

    1.0.3 cambios en registro y registro.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
+  usuario = new FormGroup({
+    rut: new FormControl('', [Validators.required, Validators.pattern("^[0-9]{7,8}-[0-9Kk]{1}$"), this.validarRUT()]),
+    nombre: new FormControl('', [Validators.required, Validators.pattern("[A-Za-z]{3,20}")]),
+    apellido: new FormControl('', [Validators.required, Validators.pattern("[A-Za-z]{3,20}")]),
+    genero: new FormControl('', [Validators.required]), 
+    fecha_nacimiento: new FormControl('', [Validators.required]),
+    correo_electronico: new FormControl('', [Validators.required, Validators.email]),
+    contraseña: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]),
+    confirmarContraseña: new FormControl('', [Validators.required]),
+
+    tiene_auto: new FormControl(false),
+    patente: new FormControl('', []),
+    capacidad_asientos: new FormControl('', []),
+
+  })
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  validarRUT(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const rutValue = control.value;
+      if (!rutValue) return null;
+
+      const [rutBody, dvIngresado] = rutValue.split('-');
+      if (!rutBody || !dvIngresado) return { invalidRUT: true };
+
+      const dvCalculado = this.calcularDigitoVerificador(rutBody);
+      return dvCalculado.toLowerCase() === dvIngresado.toLowerCase() ? null : { invalidRUT: true };
+    };
+  }
+
+  calcularDigitoVerificador(rut: string): string {
+    let suma = 0;
+    let multiplicador = 2;
+
+    for (let i = rut.length - 1; i >= 0; i--) {
+      suma += parseInt(rut[i], 10) * multiplicador;
+      multiplicador = multiplicador === 7 ? 2 : multiplicador + 1;
+    }
+
+    const resto = 11 - (suma % 11);
+    if (resto === 11) return '0';
+    if (resto === 10) return 'K';
+    return resto.toString();
+  }
 }
