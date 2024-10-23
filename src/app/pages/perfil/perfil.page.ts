05e0deada8a2aa40d975313f132d292f0d217b72
vicenticko <vi.gilbert@duocuc.cl>
2024-10-22 23:11:20 -0300

    1.0.24 foto de perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
+  usuario: any;  
+
+  profileImage: string = 'assets/images/pordefectoperfil.png';
+
   constructor() { }
 
   ngOnInit() {
+    this.usuario = JSON.parse(localStorage.getItem("usuario") || '');
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.profileImage = e.target?.result as string; // Actualiza la imagen de perfil
+      };
+      reader.readAsDataURL(file);
+    }
   }
 
 }
