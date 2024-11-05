02c497f31b565d66314bdfdcf13ae9cf467663d2
vicenticko <vi.gilbert@duocuc.cl>
2024-11-05 12:49:04 -0300

    1.1.1 firebase, falta el formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule, 
-    IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB_ADfAO4LBtDIU6eDK5-FE4qo9q3FNeCA",
+    authDomain: "liftme-2a4be.firebaseapp.com",
+    projectId: "liftme-2a4be",
+    storageBucket: "liftme-2a4be.firebasestorage.app",
+    messagingSenderId: "633980134110",
+    appId: "1:633980134110:web:5bb83a456c11878880d84f"
+  }
 };
 
 /*
